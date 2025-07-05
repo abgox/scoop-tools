d2e68d3034482d1b19e9919ebf906ea0b7d175f0
abgox <abgohxf@outlook.com>
2025-07-05 15:45:02 +0800

    v1.0.0

--- scoop-install.ps1
+++ scoop-install.ps1
@@ -1,10 +1,47 @@
 ﻿param(
-    [switch]$reset,
-    [string]$app,
     [Parameter(ValueFromRemainingArguments = $true)]
     [string[]]$restArgs
 )
-function ConvertFrom_JsonToHashtable {
+
+$version = "1.0.0"
+
+$showCN = $PSUICulture -like 'zh*'
+
+if (!$restArgs) {
+    Write-Host "scoop-install " -ForegroundColor Magenta -NoNewline
+    Write-Host "v$version" -ForegroundColor Cyan
+    if ($showCN) {
+        Write-Host "一个 PowerShell 脚本，它允许你添加 Scoop 配置，在 Scoop 安装应用时使用替换后的 url 而不是原始的 url。" -ForegroundColor Cyan
+        Write-Host "详情请查看: " -ForegroundColor Cyan -NoNewline
+    }
+    else {
+        Write-Host "A PowerShell script that allows you to add Scoop configurations to use a replaced url instead of the original url when installing the app in Scoop." -ForegroundColor Cyan
+        Write-Host "For more information, please visit: " -ForegroundColor Cyan -NoNewline
+    }
+    Write-Host "https://gitee.com/abgox/scoop-install" -ForegroundColor Blue -NoNewline
+    Write-Host " 或 " -ForegroundColor Cyan -NoNewline
+    Write-Host "https://github.com/abgox/scoop-install" -ForegroundColor Blue
+    return
+}
+
+$appList = @()
+$ScoopParams = @()
+
+foreach ($arg in $restArgs) {
+    if ($arg -like "-*") {
+        if ($arg -eq "-reset") {
+            $reset = $true
+        }
+        else {
+            $ScoopParams += $arg
+        }
+    }
+    else {
+        $appList += $arg.Trim()
+    }
+}
+
+function ConvertFrom-JsonToHashtable {
     param(
         [Parameter(ValueFromPipeline = $true)]
         [string]$json
@@ -73,12 +110,43 @@ function Replace-Multiple {
     return $InputString
 }
 
-$app = $app.Trim()
-$config = scoop config
-$current_path = Get-Location
+try {
+    $config = scoop config
+}
+catch {
+    if ($showCN) {
+        Write-Host "获取 scoop 配置失败，请检查 scoop 是否正常安装" -ForegroundColor Red
+    }
+    else {
+        Write-Host "Failed to get scoop configuration. Please check if scoop is properly installed." -ForegroundColor Red
+    }
+    return
+}
+$currentPath = Get-Location
+$origin = $config.'scoop-install-url-replace-from'
+$replace = $config.'scoop-install-url-replace-to'
+$originPatterns = $origin.Split('|||')
+$replacePatterns = $replace.Split('|||')
+
+if ($reset) {
+    if ($showCN) {
+        Write-Host "正在通过 git stash 撤销以下 scoop bucket 中的本地文件更改:" -ForegroundColor Green
+    }
+    else {
+        Write-Host "Undoing local file changes in the following scoop buckets by git stash:" -ForegroundColor Green
+    }
+
+    Get-ChildItem "$($config.root_path)\buckets" | ForEach-Object {
+        Set-Location $_.FullName
+        Write-Host $_.FullName -ForegroundColor Cyan -NoNewline
+        Write-Host ": " -NoNewline
+        git stash -m "stash changes via scoop-install ($(Get-Date))"
+    }
+    Set-Location $currentPath
+}
 
 if ($null -eq $config.root_path) {
-    if ($PSUICulture -like "zh*") {
+    if ($showCN) {
         Write-Host "你还没有设置 scoop 的根目录。" -ForegroundColor Yellow
         Write-Host "参考配置:" -ForegroundColor Cyan
         Write-Host 'scoop config root_path "D:\scoop"' -ForegroundColor Cyan
@@ -91,107 +159,82 @@ if ($null -eq $config.root_path) {
     return
 }
 
-if ($reset) {
-    if ($PSUICulture -like "zh*") {
-        Write-Host "正在清除以下 scoop bucket 中的本地文件更改:" -ForegroundColor Green
+if ($origin -and $replace) {
+    $hasConfig = $true
+}
+else {
+    if ($showCN) {
+        Write-Host '你还没有添加相关配置。' -ForegroundColor Yellow
+        Write-Host "详情请查看: " -ForegroundColor Cyan -NoNewline
+        Write-Host "https://gitee.com/abgox/scoop-install" -ForegroundColor Blue -NoNewline
+        Write-Host " 或 " -ForegroundColor Cyan -NoNewline
+        Write-Host "https://github.com/abgox/scoop-install" -ForegroundColor Blue
+        Write-Host '参考配置:' -ForegroundColor Cyan
     }
     else {
-        Write-Host "Clearing local file changes in the following scoop buckets:" -ForegroundColor Green
+        Write-Host "You haven't added the relevant configuration yet." -ForegroundColor Yellow
+        Write-Host "For more information, please visit: " -ForegroundColor Cyan -NoNewline
+        Write-Host "https://gitee.com/abgox/scoop-install" -ForegroundColor Blue -NoNewline
+        Write-Host " or " -ForegroundColor Cyan -NoNewline
+        Write-Host "https://github.com/abgox/scoop-install" -ForegroundColor Blue
+        Write-Host 'Example:' -ForegroundColor Cyan
     }
+    Write-Host 'scoop config scoop-install-url-replace-from "^https://github.com|||^https://raw.githubusercontent.com"' -ForegroundColor Cyan
+    Write-Host 'scoop config scoop-install-url-replace-to "https://gh-proxy.com/github.com|||https://gh-proxy.com/raw.githubusercontent.com"' -ForegroundColor Cyan
 
-    Get-ChildItem "$($config.root_path)\buckets" | ForEach-Object {
-        Set-Location $_.FullName
-        Write-Host $_.FullName -ForegroundColor Cyan
-        git checkout --quiet .
-    }
-    Set-Location $current_path
-}
-
-if ($app -eq "") {
-    if (!$reset) {
-        Write-Host "scoop-install " -ForegroundColor Magenta -NoNewline
-        if ($PSUICulture -like "zh*") {
-            Write-Host "是一个 PowerShell 脚本，它允许你添加 Scoop 配置，在 Scoop 安装应用时使用替换后的 url 而不是原始的 url。" -ForegroundColor Cyan
-            Write-Host "详情请查看: " -ForegroundColor Cyan -NoNewline
-        }
-        else {
-            Write-Host "is a PowerShell script that allows you to add Scoop configurations to use a replaced url instead of the original url when installing the app in Scoop." -ForegroundColor Cyan
-            Write-Host "For more information, please visit: " -ForegroundColor Cyan -NoNewline
-        }
-        Write-Host "https://gitee.com/abgox/scoop-install" -ForegroundColor Blue
-    }
+    $hasConfig = $false
     return
 }
 
-try {
+foreach ($app in $appList) {
+    $hasError = $false
+
     try {
-        $info = scoop info $app
-        $bucket_path = "$($config.root_path)\buckets\$($info.Bucket)"
-        $appName = $info.Name
+        try {
+            $info = scoop info $app
+            $bucketPath = "$($config.root_path)\buckets\$($info.Bucket)"
+            $appname = $info.Name
 
-        if ($null -eq $info.Bucket -or $null -eq $info.Name) {
-            throw
+            if ($null -eq $info.Bucket -or $null -eq $info.Name) {
+                throw
+            }
         }
-    }
-    catch {
-        throw "App not found or error manifest file."
-    }
+        catch {
+            $hasError = $true
+            throw "App not found or error manifest file."
+        }
 
-    $manifest_path = (Get-ChildItem "$bucket_path\bucket" -Recurse -Filter "$appName.json").FullName
+        $manifestPath = (Get-ChildItem "$bucketPath\bucket" -Recurse -Filter "$appname.json").FullName
 
-    $origin = $config.'scoop-install-url-replace-from'
-    $replace = $config.'scoop-install-url-replace-to'
-
-    $has_config = $true
-
-    if ($null -eq $origin -or $null -eq $replace) {
-        if ($PSUICulture -like "zh*") {
-            Write-Host '你还没有添加相关配置。' -ForegroundColor Yellow
-            Write-Host '参考配置:' -ForegroundColor Cyan
+        if ($PSEdition -eq 'Desktop') {
+            $manifest = Get-Content $manifestPath -Raw | ConvertFrom-JsonToHashtable
         }
         else {
-            Write-Host "You haven't added the relevant configuration yet." -ForegroundColor Yellow
-            Write-Host 'Example:' -ForegroundColor Cyan
+            $manifest = Get-Content $manifestPath -Raw | ConvertFrom-Json -AsHashtable
         }
-        Write-Host 'scoop config scoop-install-url-replace-from "^https://github.com|||^https://raw.githubusercontent.com"' -ForegroundColor Cyan
-        Write-Host 'scoop config scoop-install-url-replace-to "https://gh-proxy.com/github.com|||https://gh-proxy.com/raw.githubusercontent.com"' -ForegroundColor Cyan
 
-        $has_config = $false
-        return
-    }
+        if ($manifest.url) {
+            $manifest.url = Replace-Multiple $manifest.url $originPatterns $replacePatterns
+        }
+        if ($manifest.architecture.'64bit'.url) {
+            $manifest.architecture.'64bit'.url = Replace-Multiple $manifest.architecture.'64bit'.url $originPatterns $replacePatterns
+        }
+        if ($manifest.architecture.'32bit'.url) {
+            $manifest.architecture.'32bit'.url = Replace-Multiple $manifest.architecture.'32bit'.url $originPatterns $replacePatterns
+        }
+        if ($manifest.architecture.arm64.url) {
+            $manifest.architecture.arm64.url = Replace-Multiple $manifest.architecture.arm64.url $originPatterns $replacePatterns
+        }
 
-    if ($PSEdition -eq 'Desktop') {
-        $manifest = Get-Content $manifest_path -Raw | ConvertFrom_JsonToHashtable
-    }
-    else {
-        $manifest = Get-Content $manifest_path -Raw | ConvertFrom-Json -AsHashtable
-    }
+        $manifest | ConvertTo-Json -Depth 100 | Out-File $manifestPath -Encoding utf8 -Force
 
-    $originPatterns = $origin.Split('|||')
-    $replacePatterns = $replace.Split('|||')
-
-
-    if ($manifest.url) {
-        $manifest.url = Replace-Multiple $manifest.url $originPatterns $replacePatterns
+        scoop install $app @ScoopParams
     }
-    if ($manifest.architecture.'64bit'.url) {
-        $manifest.architecture.'64bit'.url = Replace-Multiple $manifest.architecture.'64bit'.url $originPatterns $replacePatterns
-    }
-    if ($manifest.architecture.'32bit'.url) {
-        $manifest.architecture.'32bit'.url = Replace-Multiple $manifest.architecture.'32bit'.url $originPatterns $replacePatterns
-    }
-    if ($manifest.architecture.arm64.url) {
-        $manifest.architecture.arm64.url = Replace-Multiple $manifest.architecture.arm64.url $originPatterns $replacePatterns
-    }
-
-    $manifest | ConvertTo-Json -Depth 100 | Out-File $manifest_path -Encoding utf8 -Force
-
-    scoop install $app @restArgs
-}
-finally {
-    if ($has_config) {
-        Set-Location $bucket_path
-        git checkout -- $manifest_path
-        Set-Location $current_path
+    finally {
+        if ($hasConfig -and !$hasError) {
+            Set-Location $bucketPath
+            git checkout -- $manifestPath
+            Set-Location $currentPath
+        }
     }
 }
