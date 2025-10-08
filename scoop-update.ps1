fadd7a2918f927d103d98e28671f433927fbb2d2
abgox <abgohxf@outlook.com>
2025-10-08 15:44:43 +0800

    refactor(scoop-update): optimize the JSON conversion function to support different PowerShell versions

--- scoop-update.ps1
+++ scoop-update.ps1
@@ -64,72 +64,83 @@ if ($appList.Length -eq 0) {
     exit 1
 }
 
-function ConvertFrom-JsonAsHashtable {
-    param(
-        [Parameter(ValueFromPipeline = $true)]
-        $InputObject
-    )
-
-    begin {
-        $buffer = [System.Text.StringBuilder]::new()
+if ($PSEdition -eq 'Core') {
+    function ConvertFrom-JsonAsHashtable {
+        param(
+            [Parameter(ValueFromPipeline = $true)]
+            $InputObject
+        )
+        ConvertFrom-Json $InputObject -AsHashtable
     }
+}
+else {
+    function ConvertFrom-JsonAsHashtable {
+        param(
+            [Parameter(ValueFromPipeline = $true)]
+            $InputObject
+        )
 
-    process {
-        if ($InputObject -is [array]) {
-            $null = $buffer.Append(($InputObject -join "`n"))
-        }
-        else {
-            $null = $buffer.Append($InputObject)
-        }
-    }
-
-    end {
-        $jsonString = $buffer.ToString()
-
-        if ($PSVersionTable.PSVersion.Major -ge 7) {
-            return $jsonString | ConvertFrom-Json -AsHashtable
+        begin {
+            $buffer = [System.Text.StringBuilder]::new()
         }
 
-        $jsonString = [regex]::Replace($jsonString, '(?<!\\)"\s*"\s*:', {
-                param($m)
-                '"emptyKey_' + [Guid]::NewGuid() + '":'
-            })
-
-        $jsonString = [regex]::Replace($jsonString, ',\s*(?=[}\]]\s*$)', '')
-
-        function ProcessArray {
-            param($array)
-            $nestedArr = [System.Collections.ArrayList]::new()
-            foreach ($item in $array) {
-                if ($item -is [System.Collections.IEnumerable] -and $item -isnot [string]) {
-                    $null = $nestedArr.Add((ProcessArray $item))
-                }
-                elseif ($item -is [System.Management.Automation.PSCustomObject]) {
-                    $null = $nestedArr.Add((ConvertToHashtable $item))
-                }
-                else {
-                    $null = $nestedArr.Add($item)
-                }
+        process {
+            if ($InputObject -is [array]) {
+                $null = $buffer.Append(($InputObject -join "`n"))
             }
-            return $nestedArr
+            else {
+                $null = $buffer.Append($InputObject)
+            }
         }
 
-        function ConvertToHashtable {
-            param($obj)
-            if ($obj -is [Array]) {
-                return @($obj | ForEach-Object { ConvertToHashtable $_ })
+        end {
+            $jsonString = $buffer.ToString()
+
+            if ($PSVersionTable.PSVersion.Major -ge 7) {
+                return $jsonString | ConvertFrom-Json -AsHashtable
             }
-            elseif ($obj -is [PSCustomObject]) {
-                $h = @{}
-                foreach ($prop in $obj.PSObject.Properties) {
-                    $h[$prop.Name] = ConvertToHashtable $prop.Value
+
+            $jsonString = [regex]::Replace($jsonString, '(?<!\\)"\s*"\s*:', {
+                    param($m)
+                    '"emptyKey_' + [Guid]::NewGuid() + '":'
+                })
+
+            $jsonString = [regex]::Replace($jsonString, ',\s*(?=[}\]]\s*$)', '')
+
+            function ProcessArray {
+                param($array)
+                $nestedArr = [System.Collections.ArrayList]::new()
+                foreach ($item in $array) {
+                    if ($item -is [System.Collections.IEnumerable] -and $item -isnot [string]) {
+                        $null = $nestedArr.Add((ProcessArray $item))
+                    }
+                    elseif ($item -is [System.Management.Automation.PSCustomObject]) {
+                        $null = $nestedArr.Add((ConvertToHashtable $item))
+                    }
+                    else {
+                        $null = $nestedArr.Add($item)
+                    }
                 }
-                return $h
+                return $nestedArr
             }
-            else { return $obj }
-        }
 
-        ConvertToHashtable ($jsonString | ConvertFrom-Json)
+            function ConvertToHashtable {
+                param($obj)
+                if ($obj -is [Array]) {
+                    return @($obj | ForEach-Object { ConvertToHashtable $_ })
+                }
+                elseif ($obj -is [PSCustomObject]) {
+                    $h = @{}
+                    foreach ($prop in $obj.PSObject.Properties) {
+                        $h[$prop.Name] = ConvertToHashtable $prop.Value
+                    }
+                    return $h
+                }
+                else { return $obj }
+            }
+
+            ConvertToHashtable ($jsonString | ConvertFrom-Json)
+        }
     }
 }
 
